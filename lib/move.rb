8e2dff3b9367d5901fb2e4cbb46153571e6e46e8
lucas montoni <lmontoni@outlook.com>
2018-08-07 14:12:33 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+put "Welcome to Tic Tac Toe!"
+board = [" "," "," "," "," "," "," "," "," "]
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+ 
+update_array_at_with(board, 0, "X")
+# The element at index 0 of array 'board' is set to the value "X"
+board #=> ["X", " ", " "]
